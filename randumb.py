0751fd13dfec452a554a03a60e431993ce3a3140
kofoednielsen <asbjorn.ko.ni@gmail.com>
2019-11-25 23:05:54 +0100

    Now all errors are handled with a 200 OK status code, and a poop

--- randumb.py
+++ randumb.py
@@ -1,5 +1,5 @@
-
-from flask import Flask, redirect
+from flask import Flask, redirect, json
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
@@ -14,5 +14,12 @@ def get_random_url() -> str:
     return "https://shodan.io/"
 
 
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """ Prints poop in case of error """
+    return "<h1>💩</h1>"
+
+
 if __name__ == '__main__':
     app.run()
+
